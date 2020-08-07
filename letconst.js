c54b1a6c7c17c27e22c07ed6afb1344263f669d9
Mowmita Ahmed <ahmedmowmita@gmail.com>
2020-08-07 19:57:04 +0600

    LetConst

---
+++ letconst.js
@@ -0,0 +1,19 @@
+const hubby = "Elias Kanchon";
+console.log(hubby);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+console.log(numbers);
+
+const nayok = {name: "Shakib Khan", phone: 4678}
+
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khala";
+console.log(patientName);
+
+let sum = 0;
+for (var i = 0; i < 10; i++) {
+    sum = sum + i;
+}
+console.log(sum);

2bd19d7ded70503bd8ed5b4ab8ac038ee97eba3f
Mowmita Ahmed <ahmedmowmita@gmail.com>
2020-08-07 21:45:06 +0600

    Template String

---
+++ template.js
@@ -0,0 +1,12 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName +" "+ lastName + " is a good boy.";
+const fullName2 = `${firstName} ${20 + 50+30} is a good boy.`;
+console.log(fullName2);
+
+const multiline = `I Love you
+I miss you
+no.I don't need you.
+Baily road e dorkar nai.`
+
+console.log(multiline);
